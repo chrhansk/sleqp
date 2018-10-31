cdc0555e5a0c2aafc214716f274f33a01ba8661d
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2018-10-31 16:10:39 +0100

    Added missing file

---
+++ src/main/sleqp_util.h
@@ -0,0 +1,42 @@
+#ifndef SLEQP_UTIL_H
+#define SLEQP_UTIL_H
+
+#include "sleqp_func.h"
+#include "sleqp_iterate.h"
+#include "sleqp_problem.h"
+#include "sleqp_types.h"
+
+#include "sparse/sleqp_sparse_matrix.h"
+#include "sparse/sleqp_sparse_vec.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  SLEQP_RETCODE sleqp_set_and_evaluate(SleqpProblem* problem,
+                                       SleqpIterate* iterate);
+
+  SLEQP_RETCODE sleqp_get_violated_multipliers(SleqpProblem* problem,
+                                               SleqpSparseVec* x,
+                                               SleqpSparseVec* cons_vals,
+                                               double penalty_parameter,
+                                               SleqpSparseVec* multipliers,
+                                               SleqpActiveSet* active_set,
+                                               double eps);
+
+  SLEQP_RETCODE sleqp_max_step_length(SleqpSparseVec* x,
+                                      SleqpSparseVec* direction,
+                                      SleqpSparseVec* var_lb,
+                                      SleqpSparseVec* var_ub,
+                                      double* max_step_length);
+
+  SLEQP_RETCODE sleqp_get_violation(SleqpProblem* problem,
+                                    SleqpIterate* iterate,
+                                    double eps,
+                                    SleqpSparseVec* violation);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SLEQP_UTIL_H */
