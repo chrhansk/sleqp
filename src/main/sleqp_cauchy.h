e8aa397ea2a967c7cd414e28d68f2157bff05190
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2020-09-09 15:39:23 +0200

    Added documentation

--- src/main/sleqp_cauchy.h
+++ src/main/sleqp_cauchy.h
@@ -25,22 +25,62 @@ extern "C" {
 
   SLEQP_RETCODE sleqp_cauchy_data_free(SleqpCauchyData** star);
 
+  /**
+   * Sets the iterate and trust radius for the current LP.
+   *
+   * @param[in]       cauchy_data        Cauchy data
+   * @param[in]       iterate            The current iterate
+   * @param[in]       trust_radius       The trust radius
+   **/
   SLEQP_RETCODE sleqp_cauchy_set_iterate(SleqpCauchyData* cauchy_data,
                                          SleqpIterate* iterate,
                                          double trust_radius);
 
+  /**
+   * Solve the LP according to the current iterate. The
+   * objective is determined according to the function
+   * gradient and penalty value.
+   *
+   * @param[in]       cauchy_data        Cauchy data
+   * @param[in]       gradient           The objective value gradient
+   * @param[in]       penalty            The penalty value
+   *
+   **/
   SLEQP_RETCODE sleqp_cauchy_solve(SleqpCauchyData* cauchy_data,
                                    SleqpSparseVec* gradient,
                                    double penalty);
 
+  /**
+   * Updates the working of the given iterate according to the optimal basis of
+   * the current LP.
+   *
+   * @param[in]       cauchy_data        Cauchy data
+   * @param[out]      iterate            The current iterate
+   * @param[in]       trust_radius       The trust radius
+   *
+   **/
   SLEQP_RETCODE sleqp_cauchy_get_working_set(SleqpCauchyData* cauchy_data,
                                              SleqpIterate* iterate,
                                              double trust_radius);
 
+  /**
+   * Returns the Cauchy direction according to the current LP solution.
+   *
+   * @param[in]       cauchy_data        Cauchy data
+   * @param[out]      direction          The Cauchy direction
+   *
+   **/
   SLEQP_RETCODE sleqp_cauchy_get_direction(SleqpCauchyData* cauchy_data,
-                                           SleqpIterate* iterate,
                                            SleqpSparseVec* direction);
 
+  /**
+   * Returns the dual estimation according to duals of the current LP solution. Duals
+   * will be stored in the given iterate.
+   *
+   * @param[in]       cauchy_data        Cauchy data
+   * @param[out]      iterate            The current iterate
+   *
+   **/
   SLEQP_RETCODE sleqp_cauchy_get_dual_estimation(SleqpCauchyData* cauchy_data,
                                                  SleqpIterate* iterate);
 
@@ -48,6 +88,9 @@ extern "C" {
    * Gets the total constraint vioation of the solution at the given iterate.
    * The total violation is defined as the sum over the violations of all
    * violated constraints.
+   *
+   * @param[in]       cauchy_data        Cauchy data
+   * @param[out]      violation          The total constraint violation
    **/
   SLEQP_RETCODE sleqp_cauchy_get_violation(SleqpCauchyData* cauchy_data,
                                            double* violation);
