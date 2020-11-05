6db07a8260dcfaead6c538bfe2af3ef47ddc9c79
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2020-11-05 22:33:12 +0100

    Added support for MUMPS

---
+++ src/main/sparse/sleqp_sparse_factorization_mumps.h
@@ -0,0 +1,19 @@
+#ifndef SLEQP_SPARSE_FACTORIZATION_MUMPS_H
+#define SLEQP_SPARSE_FACTORIZATION_MUMPS_H
+
+#include "sleqp_types.h"
+#include "sleqp_sparse_matrix.h"
+#include "sleqp_sparse_factorization.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  SLEQP_RETCODE sleqp_sparse_factorization_mumps_create(SleqpSparseFactorization** star,
+                                                        SleqpParams* params);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SLEQP_SPARSE_FACTORIZATION_MUMPS_H */
