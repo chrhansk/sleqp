03b7cdd404ed396b7238f13d38e8057d509cae3a
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2020-11-24 17:11:12 +0100

    Improved formatting

--- src/main/sleqp_solver.c
+++ src/main/sleqp_solver.c
@@ -1141,7 +1141,7 @@ static SLEQP_RETCODE print_line(SleqpSolver* solver)
   {
     snprintf(jac_condition_buf,
              1024,
-             "%f",
+             "%14e",
              aug_jac_condition);
   }
   else
