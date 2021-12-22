ca5333041de3645659500b52f803af4df32908db
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2021-12-22 12:58:24 +0100

    Implemented Hessian products

--- bindings/mex/mex_func.c
+++ bindings/mex/mex_func.c
@@ -22,12 +22,20 @@
 
 typedef struct
 {
+  // Callbacks
   mxArray* objective;
   mxArray* gradient;
   mxArray* constraints;
   mxArray* jacobian;
+  mxArray* hessian;
 
   mxArray* primal;
+
+  mxArray* obj_dual;
+  mxArray* cons_dual;
+
+  double* direction;
+  double* product;
 
 } FuncData;
 
@@ -173,6 +181,50 @@ mex_func_cons_jac(SleqpFunc* func,
 }
 
 static SLEQP_RETCODE
+hess_prod(const mxArray* hessian, const double* direction, double* product)
+{
+  const int dim = mxGetN(hessian);
+
+  for (int row = 0; row < dim; ++row)
+  {
+    product[row] = 0.;
+  }
+
+  const mwIndex* jc = mxGetJc(hessian);
+  const mwIndex* ir = mxGetIr(hessian);
+  const double* pr  = mxGetPr(hessian);
+
+  assert(jc[0] == 0);
+
+  mwIndex index = 0;
+
+  for (mwIndex col = 0; col < dim; ++col)
+  {
+    assert(jc[col] <= jc[col + 1]);
+
+    for (; index < jc[col + 1]; ++index)
+    {
+      const mwIndex row  = ir[index];
+      const double value = pr[index];
+
+      assert(row >= col);
+
+      if (row == col)
+      {
+        product[row] += value * direction[col];
+      }
+      else
+      {
+        product[row] += value * direction[col];
+        product[col] += value * direction[row];
+      }
+    }
+  }
+
+  return SLEQP_OKAY;
+}
+
+static SLEQP_RETCODE
 mex_func_hess_prod(SleqpFunc* func,
                    const double* obj_dual,
                    const SleqpSparseVec* direction,
@@ -180,14 +232,52 @@ mex_func_hess_prod(SleqpFunc* func,
                    SleqpSparseVec* result,
                    void* data)
 {
+  FuncData* func_data = (FuncData*)data;
+
+  SLEQP_CALL(
+    sleqp_sparse_vector_to_raw(cons_duals, mxGetPr(func_data->cons_dual)));
+
+  if (obj_dual)
+  {
+    *mxGetPr(func_data->obj_dual) = *obj_dual;
+  }
+  else
+  {
+    *mxGetPr(func_data->obj_dual) = 0.;
+  }
+
+  mxArray* lhs;
+  mxArray* rhs[] = {func_data->hessian,
+                    func_data->primal,
+                    func_data->obj_dual,
+                    func_data->cons_dual};
+
+  MEX_CALL(mexCallMATLABWithTrap(1, &lhs, 4, rhs, "feval"));
+
+  assert(mxIsDouble(lhs));
+  assert(!mxIsComplex(lhs));
+  assert(mxIsSparse(lhs));
+
+  const int num_vars = sleqp_func_num_vars(func);
+
+  assert(mxGetM(lhs) == num_vars);
+  assert(mxGetN(lhs) == num_vars);
+
+  SLEQP_CALL(sleqp_sparse_vector_to_raw(direction, func_data->direction));
+
+  SLEQP_CALL(hess_prod(lhs, func_data->direction, func_data->product));
+
+  SLEQP_CALL(
+    sleqp_sparse_vector_from_raw(result, func_data->product, num_vars, 0.));
+
   return SLEQP_OKAY;
 }
 
 static SLEQP_RETCODE
 create_func_data(FuncData** star,
                  const mxArray* callbacks,
-                 int num_variables,
-                 int num_constraints)
+                 int num_vars,
+                 int num_cons)
 {
   SLEQP_CALL(sleqp_malloc(star));
 
@@ -201,8 +291,15 @@ create_func_data(FuncData** star,
   func_data->gradient    = mxGetField(callbacks, 0, "gradient");
   func_data->constraints = mxGetField(callbacks, 0, "constraints");
   func_data->jacobian    = mxGetField(callbacks, 0, "jacobian");
+  func_data->hessian     = mxGetField(callbacks, 0, "hessian");
 
-  func_data->primal = mxCreateDoubleMatrix(1, num_variables, mxREAL);
+  func_data->primal = mxCreateDoubleMatrix(1, num_vars, mxREAL);
+
+  func_data->obj_dual  = mxCreateDoubleScalar(0.);
+  func_data->cons_dual = mxCreateDoubleMatrix(num_cons, 1, mxREAL);
+
+  SLEQP_CALL(sleqp_alloc_array(&func_data->direction, num_vars));
+  SLEQP_CALL(sleqp_alloc_array(&func_data->product, num_vars));
 
   return SLEQP_OKAY;
 }
@@ -211,6 +308,13 @@ static SLEQP_RETCODE
 mex_func_free(void* data)
 {
   FuncData* func_data = (FuncData*)data;
+
+  sleqp_free(&func_data->product);
+  sleqp_free(&func_data->direction);
+
+  mxDestroyArray(func_data->cons_dual);
+  mxDestroyArray(func_data->obj_dual);
+  mxDestroyArray(func_data->primal);
 
   sleqp_free(&func_data);
 
