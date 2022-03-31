a2f458c92e42300c412fde198a02c9595a00d258
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2022-03-31 22:13:43 +0200

    Add timer to LSQ functions

--- src/main/lsq.h
+++ src/main/lsq.h
@@ -4,6 +4,7 @@
 #include "pub_lsq.h"
 
 #include "func.h"
+#include "timer.h"
 
 double
 sleqp_lsq_func_get_levenberg_marquardt(SleqpFunc* func);
@@ -23,6 +24,15 @@ SLEQP_NODISCARD SLEQP_RETCODE
 sleqp_lsq_func_jac_adjoint(SleqpFunc* func,
                            const SleqpVec* adjoint_direction,
                            SleqpVec* product);
+
+SleqpTimer*
+sleqp_lsq_func_residual_timer(SleqpFunc* func);
+
+SleqpTimer*
+sleqp_lsq_func_adjoint_timer(SleqpFunc* func);
+
+SleqpTimer*
+sleqp_lsq_func_forward_timer(SleqpFunc* func);
 
 void*
 sleqp_lsq_func_get_data(SleqpFunc* func);
