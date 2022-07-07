4d62d07d763a8840525b32d4e83b5b95a391ec0e
Christoph Hansknecht <c.hansknecht@tu-braunschweig.de>
2022-07-07 10:23:31 +0200

    Fix error in install example

--- src/test/install/example.c
+++ src/test/install/example.c
@@ -17,15 +17,8 @@ test_set(SleqpFunc* func,
          SleqpVec* x,
          SLEQP_VALUE_REASON reason,
          bool* reject,
-         int* obj_grad_nnz,
-         int* cons_val_nnz,
-         int* cons_jac_nnz,
          void* func_data)
 {
-  *obj_grad_nnz = 0;
-  *cons_val_nnz = 0;
-  *cons_jac_nnz = 0;
-
   return SLEQP_OKAY;
 }
 
